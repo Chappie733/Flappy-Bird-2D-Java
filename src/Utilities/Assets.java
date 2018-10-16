187af3748cadd3ea4eb136189057c04873fae6e4
Chappie733 <41633580+Chappie733@users.noreply.github.com>
2018-10-16 18:27:32 +0200

    Update Assets.java

--- src/Utilities/Assets.java
+++ src/Utilities/Assets.java
@@ -4,15 +4,15 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
 	
-	private static BufferedImage source = Loader.LoadImage("Images.png");
+	private static BufferedImage source = Loader.LoadImage("Images.png"); // Loads the image to then get all the others
 	
 	public static BufferedImage bird, brick, Won;
 	
 	public static void init() {
 		
-		bird = source.getSubimage(0, 0, 64, 64);
-		brick = source.getSubimage(64, 0, 64, 150);
-		Won = Loader.LoadImage("Win.png");
+		bird = source.getSubimage(0, 0, 64, 64); // the player/bird image
+		brick = source.getSubimage(64, 0, 64, 150); // the obstacle/tube image
+		Won = Loader.LoadImage("Win.png"); // the image that displays when you win (at 60 points)
 		
 	}
 }
