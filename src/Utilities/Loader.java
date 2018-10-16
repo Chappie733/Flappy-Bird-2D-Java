8de1207f6d0c6fb5ad682db1776155fc326ef5b5
Chappie733 <41633580+Chappie733@users.noreply.github.com>
2018-10-16 18:39:43 +0200

    Update Loader.java

--- src/Utilities/Loader.java
+++ src/Utilities/Loader.java
@@ -8,10 +8,10 @@ public class Loader {
 	
 	public static BufferedImage LoadImage(String path) {
 		try {
-			return ImageIO.read(Loader.class.getResource("/res/" + path));
+			return ImageIO.read(Loader.class.getResource("/res/" + path)); // reads the texture from the resource folder and returns it
 		} catch (IOException e) {
-			e.printStackTrace();
+			e.printStackTrace(); // in case of an error, print it in the console
 		}
-		return null;
-	}
+		return null; // just added this to make sure the compilation doesn't crash, even if this is never gonna be reached,
+	} // cause if the resource is null the game is just gonna crash, so it will never come to this return null;
 }
