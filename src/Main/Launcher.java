35a8aadadf650c8cc2273245dd3bf9da8ee3556d
Chappie733 <41633580+Chappie733@users.noreply.github.com>
2018-10-16 18:41:02 +0200

    Update Launcher.java

--- src/Main/Launcher.java
+++ src/Main/Launcher.java
@@ -2,7 +2,7 @@ package Main;
 
 public class Launcher {
 	public static void main(String[] args) {
-		Game game = new Game(800, 600, "Flappy Bird");
-		game.start();
+		Game game = new Game(800, 600, "Flappy Bird"); // Creates the Game class, sets the width(800), the height(600)
+		game.start(); // and the title("Flappy Bird)" to the window, and starts the game's thread/process
 	}
 }
